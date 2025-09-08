272e879071386195d7a1ea4e655ba2e065d49d52
Destiny Ifehor <destechofficial@gmail.com>
2025-09-08 23:34:43 +0100

    updated

--- src/app/(discuss)/discuss/[section]/[slugId]/[slug]/page.ts
+++ src/app/(discuss)/discuss/[section]/[slugId]/[slug]/page.ts
@@ -32,7 +32,7 @@ export async function generateMetadata({
   const previewText = post.content?.slice(0, 120) ?? 'Check out this post';
   const firstImage =
     post.images?.[0]?.secure_url ??
-    `https://discuss-mu-three.vercel.app/assets/default.jpg`;
+    `https://discuss-mu-three.vercel.app/public/wizzy.jpg`;
 
   return {
     title: `${post.title} | ${APP_NAME}`,

b643703da9343ce12a87c73a5b874bf811c91fa1
Destiny Ifehor <destechofficial@gmail.com>
2025-09-13 23:45:19 +0100

    updated

--- src/components/layouts/dashboard/mobile-navigation.tsx
+++ src/components/layouts/dashboard/mobile-navigation.tsx
@@ -46,8 +46,8 @@ const MobileNavigation: React.FC<MainLayoutProps> = ({children}) => {
     queryKey: ['unreadCount'],
     queryFn: () => getUnreadNotificationsCounntRequestAction(),
     retry: 1,
-    refetchInterval: 5000,
-    refetchIntervalInBackground: false,
+    // refetchInterval: 5000,
+    //refetchIntervalInBackground: false,
   });
 
   const isActive = (path: string) => location === path;

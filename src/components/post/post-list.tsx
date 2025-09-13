dc382e6cc684634c538496a927429bf574864be3
Destiny Ifehor <destechofficial@gmail.com>
2025-09-13 22:42:09 +0100

    updated

--- src/components/post/post-list.tsx
+++ src/components/post/post-list.tsx
@@ -63,6 +63,8 @@ export const SectionPostList = ({
     placeholderData: previousData => previousData,
     enabled: shouldQuery,
     retry: 1,
+    refetchInterval: 5000, // 👈 Poll every 5s
+    refetchIntervalInBackground: false,
   });
 
   const sectionData = useMemo(() => {

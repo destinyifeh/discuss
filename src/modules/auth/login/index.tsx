d99e19009f144ba833568e443c2ceeed93332796
Destiny Ifehor <destechofficial@gmail.com>
2025-09-13 12:26:53 +0100

    updated

--- src/modules/auth/login/index.tsx
+++ src/modules/auth/login/index.tsx
@@ -157,7 +157,7 @@ export const LoginPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center p-4">
       <div className="flex w-full md:max-w-4xl">
-        <div className="hidden md:flex flex-1 items-center justify-center rounded-l-lg p-8 text-white bg-app dark:bg-app/90">
+        <div className="hidden lg:flex flex-1 items-center justify-center rounded-l-lg p-8 text-white bg-app dark:bg-app/90">
           <div>
             {/* <svg
                 viewBox="0 0 24 24"

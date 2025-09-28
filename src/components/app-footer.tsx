ed41a11df384cf06a4942eee9cc91778ad69c19b
Destiny Ifehor <destechofficial@gmail.com>
2025-09-28 16:54:13 +0100

    updated

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,30 +1,59 @@
 'use client';
 
+import {cn} from '@/lib/utils';
 import moment from 'moment';
 import Link from 'next/link';
+import {usePathname} from 'next/navigation';
 
 export const AppFooter = () => {
+  const location = usePathname();
+  const isActive = (path: string) => location === path;
   return (
     <footer className="py-8 px-4 text-center text-app-gray border-t border-app-border">
       <div className="flex flex-wrap justify-center gap-4 max-w-4xl mx-auto mb-4">
-        <Link href="/about" className="hover:underline text-sm">
+        <Link
+          href="/about"
+          className={cn(
+            'hover:underline text-sm text-foreground',
+            isActive('/about') ? 'font-bold' : 'font-normal',
+          )}>
           About
         </Link>
-        <Link href="/help-center" className="hover:underline text-sm">
+        <Link
+          href="/help-center"
+          className={cn(
+            'hover:underline text-sm text-foreground',
+            isActive('/help-center') ? 'font-bold' : 'font-normal',
+          )}>
           Help Center
         </Link>
-        <Link href="/terms-of-service" className="hover:underline text-sm">
+        <Link
+          href="/terms-of-service"
+          className={cn(
+            'hover:underline text-sm text-foreground',
+            isActive('/terms-of-service') ? 'font-bold' : 'font-normal',
+          )}>
           Terms of Service
         </Link>
-        <Link href="/privacy-policy" className="hover:underline text-sm">
+        <Link
+          href="/privacy-policy"
+          className={cn(
+            'hover:underline text-sm text-foreground',
+            isActive('/privacy-policy') ? 'font-bold' : 'font-normal',
+          )}>
           Privacy Policy
         </Link>
 
-        <Link href="/ads-info" className="hover:underline text-sm">
+        <Link
+          href="/ads-info"
+          className={cn(
+            'hover:underline text-sm text-foreground',
+            isActive('/ads-info') ? 'font-bold' : 'font-normal',
+          )}>
           Ads info
         </Link>
       </div>
-      <p className="text-sm">
+      <p className="text-sm text-foreground">
         © {moment().format('YYYY')} Discussday. All rights reserved.
       </p>
     </footer>
